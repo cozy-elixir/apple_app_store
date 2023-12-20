b7b164cd3fdd9e289a9b2fd473d484cf522c4728
c4710n <c4710n@users.noreply.github.com>
2023-12-20 14:01:41 +0800

    init: mix new apple_app_store

---
+++ lib/apple_app_store.ex
@@ -0,0 +1,18 @@
+defmodule AppleAppStore do
+  @moduledoc """
+  Documentation for `AppleAppStore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AppleAppStore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/apple_app_store_test.exs
@@ -0,0 +1,8 @@
+defmodule AppleAppStoreTest do
+  use ExUnit.Case
+  doctest AppleAppStore
+
+  test "greets the world" do
+    assert AppleAppStore.hello() == :world
+  end
+end
